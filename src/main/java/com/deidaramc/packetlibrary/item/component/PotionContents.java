4ce8d172d18457aefcfe30c248e746a3da6a8a6b
DeidaraMC <DeidaraJuice@gmail.com>
2024-07-03 17:40:16 -0400

    fix: potion contents read/writing

--- src/main/java/com/deidaramc/packetlibrary/item/component/PotionContents.java
+++ src/main/java/com/deidaramc/packetlibrary/item/component/PotionContents.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public record PotionContents(@NotNull PotionType type, @Nullable RGBLike customColor, @NotNull List<PotionDetails> potionDetails) implements NetworkWriter {
+public record PotionContents(@Nullable PotionType type, @Nullable RGBLike customColor, @NotNull List<PotionDetails> potionDetails) implements NetworkWriter {
     public PotionContents {
         potionDetails = Collections.unmodifiableList(potionDetails);
     }
@@ -30,7 +30,9 @@ public record PotionContents(@NotNull PotionType type, @Nullable RGBLike customC
 
     @Override
     public void write(@NotNull ByteBuffer buffer) {
-        ProtocolUtil.writeVarInt(type.id(), buffer);
+        boolean hasType = type != null;
+        ProtocolUtil.writeBoolean(hasType, buffer);
+        ProtocolUtil.writeVarInt(hasType ? type.id() : 0, buffer);
         boolean hasCustomColor = customColor != null;
         ProtocolUtil.writeBoolean(hasCustomColor, buffer);
         if (hasCustomColor) buffer.putInt(RGBUtil.toInt(customColor));
@@ -40,16 +42,17 @@ public record PotionContents(@NotNull PotionType type, @Nullable RGBLike customC
     }
 
     public static @NotNull PotionContents fromByteBuffer(@NotNull ByteBuffer buffer) {
-        int typeId = ProtocolUtil.readVarInt(buffer);
+        boolean hasTypeId = ProtocolUtil.readBoolean(buffer);
+        int typeId = hasTypeId ? ProtocolUtil.readVarInt(buffer) : 0;
         boolean hasCustomColor = ProtocolUtil.readBoolean(buffer);
-        int customColor = buffer.getInt();
+        Color customColor = hasCustomColor ? new Color(buffer.getInt()) : null;
         int potionDetailCount = ProtocolUtil.readVarInt(buffer);
         if (potionDetailCount <= 0) {
-            return new PotionContents(PotionType.fromId(typeId), hasCustomColor ? new Color(customColor) : null);
+            return new PotionContents(PotionType.fromId(typeId), hasCustomColor ? customColor : null);
         }
 
         List<PotionDetails> details = new ArrayList<>(potionDetailCount);
         for (int i = 0; i < potionDetailCount; i++) details.add(PotionDetails.fromByteBuffer(buffer));
-        return new PotionContents(PotionType.fromId(typeId), hasCustomColor ? new Color(customColor) : null, details);
+        return new PotionContents(PotionType.fromId(typeId), hasCustomColor ? customColor : null, details);
     }
 }
